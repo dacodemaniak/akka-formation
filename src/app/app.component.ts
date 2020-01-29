c4da9a79a370bef6e00951c80af005c0284ca6bc
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-01-29 11:06:35 +0100

    Some operators rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, of, Subscription, from } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   private _title: String = 'Hello Angular';
+
+  private oneObservable: Observable<string> = of('a value');
+
+  public constructor() {
+    const stream: Subscription = this.oneObservable.subscribe((observed: string) => {
+      console.log('Je suis la valeur observée : ' + observed);
+    }, (error) => {
+      console.log('Something went wrong while subscription : ', error);
+    },
+    () => { // Plus aucune donnée émise par l'Observateur
+      console.log('Process finished');
+    });
+    stream.unsubscribe();
+
+    const arrayOfValues: number[] = [15, 30, 10, 9, 9];
+    const observator: Observable<number> = from(arrayOfValues);
+    observator.subscribe((observed: number) => {
+      console.log('Hey : ', observed);
+    },
+    (error) => {
+      console.log('Some errors while reading');
+    },
+    () => {
+      console.log('No more digits in array...');
+    });
+
+    // Some other operators
+    const stringObservator: Observable<string> = of("Jean-Luc");
+    let originalValue: string;
+    stringObservator.pipe(
+      tap((value) => {
+        console.log('Before : ', value);
+        originalValue = value;
+      }),
+      map((value: string) => value.length),
+      tap((value) => console.log('Après modification : ', value))
+    ).subscribe((value: number) => {
+      console.log('Value observed : ', value, " from ", originalValue);
+    });
+  }
 
   public get title(): String {
     return this._title;
